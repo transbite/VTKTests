66cbff5f2144e8e20d0f00badc3cfd746109e618
Catalin <Catalin>
2015-02-18 17:50:50 +0200

    Qt plugins tests...

---
+++ SimpleQtPlugin/SimpleQtPlugin.cpp
@@ -0,0 +1,1 @@
+#include "SimpleQtPlugin.h"
---
+++ SimpleQtPlugin/SimpleQtPlugin.h
@@ -0,0 +1,14 @@
+#ifndef _SIMPLE_QT_PLUGIN_H_
+#define _SIMPLE_QT_PLUGIN_H_
+
+#include <QObject>
+
+class SimpleQtPlugin : public QObject
+{
+public:
+
+private:
+
+};
+
+#endif //_SIMPLE_QT_PLUGIN_H_
